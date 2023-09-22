2fd8c2ea9824acab087172d4127df617979e69af
kao19 <feiyukine@gmail.com>
2023-09-23 00:16:13 +0100

    adding comments

--- backend/src/main/java/irisi/facebook/backend/security/CorsConfig.java
+++ backend/src/main/java/irisi/facebook/backend/security/CorsConfig.java
@@ -6,11 +6,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
+    // Configure Cross-Origin Resource Sharing (CORS)
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("*");
+        registry.addMapping("/**") // Allow CORS for all endpoints
+                .allowedOrigins("http://localhost:3000") // Allow requests from this origin
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // Allow these HTTP methods
+                .allowedHeaders("*"); // Allow all headers in the request
     }
 }
