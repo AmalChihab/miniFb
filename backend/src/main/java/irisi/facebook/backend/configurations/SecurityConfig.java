3b9255d5bedfbf9aa66f68909e8eb83d35d997a2
kao19 <feiyukine@gmail.com>
2023-09-22 15:40:23 +0100

    no changes

--- backend/src/main/java/irisi/facebook/backend/configurations/SecurityConfig.java
+++ backend/src/main/java/irisi/facebook/backend/configurations/SecurityConfig.java
@@ -1,2 +1,46 @@
-package irisi.facebook.backend.configurations;public class SecurityConfig {
+package irisi.facebook.backend.configurations;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/", "/public/**").permitAll()
+                .antMatchers("/private/**").authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .successHandler(defaultSuccessHandler())
+                .permitAll()
+                .and()
+                .logout()
+                .logoutSuccessUrl("/login")
+                .permitAll()
+                .and()
+                .csrf().disable();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationSuccessHandler defaultSuccessHandler() {
+        return new SimpleUrlAuthenticationSuccessHandler("/private/dashboard");
+    }
 }
--- backend/src/main/java/irisi/facebook/backend/controllers/AuthController.java
+++ backend/src/main/java/irisi/facebook/backend/controllers/AuthController.java
@@ -1,2 +1,19 @@
-package irisi.facebook.backend.controllers;public class AuthController {
+package irisi.facebook.backend.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AuthController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/private/dashboard")
+    public String dashboard() {
+        return "dashboard";
+    }
 }
+
