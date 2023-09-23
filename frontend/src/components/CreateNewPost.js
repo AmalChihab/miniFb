2ec50f8d06d5040068a073cbce119f17fb8b36ec
AmalChihab <amal.chihab3@gmail.com>
2023-09-23 14:44:28 +0100

    add frontend

---
+++ frontend/src/components/CreateNewPost.js
@@ -0,0 +1,89 @@
+// CreateNewPost.js
+import React, { useState } from 'react';
+import PostService from '../services/PostService';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPaperPlane } from '@fortawesome/free-regular-svg-icons'; 
+import defaultPhoto from '../assets/images/default.jpg'
+
+function CreateNewPost() {
+  const [postText, setPostText] = useState('');
+
+  const handlePostTextChange = (e) => {
+    setPostText(e.target.value);
+  };
+
+  const handlePostClick = () => {
+    if (postText) {
+      // Prepare the data for the new post
+      const newPostData = {
+        body: postText,
+        user: {
+            userId:1,
+            userDescription: "this is description",
+            userName:"amal",
+            userPassword:"123"
+        },
+        photo: {defaultPhoto}, 
+      };
+  
+      // Call your service's method to create a new post
+      PostService.createPost(newPostData)
+        .then((response) => {
+          // Handle the response, e.g., show a success message or update the UI
+          console.log('Post created successfully:', response);
+        })
+        .catch((error) => {
+          // Handle any errors, e.g., show an error message or log the error
+          console.error('Error creating post:', error);
+        });
+  
+      // Clear the input field after posting
+      setPostText('');
+    }
+  };
+  
+
+  const buttonStyle = {
+    backgroundColor : '#EE2C4D',
+    transition: 'background-color 0.3s ease',
+    
+  }
+
+  return (
+    <div className="w-752 h-185 bg-white p-4 mb-4 rounded-lg shadow-md mx-auto"
+    style={{ maxWidth: '752px' }}>
+    <textarea
+        className="w-full border border-gray-300 rounded-lg p-2 focus:outline-none focus:border-blue-500"
+        placeholder="What's on your mind?"
+        rows="3"
+        value={postText}
+        onChange={handlePostTextChange}
+        style={{
+          backgroundColor: '#F9F9F9', 
+          borderRadius: '10px', 
+          borderColor: 'transparent',
+        }}
+        onFocus={(e) => {
+          e.target.style.borderColor = '#EE2C4D';
+        }}
+        onBlur={(e) => {
+          e.target.style.borderColor = 'transparent'; 
+        }}
+      ></textarea>
+      <div className="flex justify-end items-center mt-2">
+      <button
+          className="text-white px-4 py-1 rounded-md  hover:bg-ee2c4d hover:text-white font-bold"
+          onClick={handlePostClick}
+          style={buttonStyle} 
+        >
+        <div className="flex items-center space-between">
+            <FontAwesomeIcon icon={faPaperPlane} />
+            <span className="ml-2">Post</span>
+          </div>
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default CreateNewPost;
---
+++ frontend/src/components/PostContent.js
@@ -0,0 +1,93 @@
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faThumbsUp, faThumbsDown, faComment } from '@fortawesome/free-solid-svg-icons';
+
+const PostContent = ({ post, user }) => {
+  const [liked, setLiked] = useState(false);
+  const [disliked, setDisliked] = useState(false);
+  const [commentText, setCommentText] = useState('');
+  const [comments, setComments] = useState([]);
+
+  const handleLikeClick = () => {
+    setLiked(!liked);
+    if (disliked) {
+      setDisliked(false);
+    }
+  };
+
+  const handleDislikeClick = () => {
+    setDisliked(!disliked);
+    if (liked) {
+      setLiked(false);
+    }
+  };
+
+  const handleCommentChange = (e) => {
+    setCommentText(e.target.value);
+  };
+
+  const handleCommentSubmit = () => {
+    if (commentText) {
+      // Add the comment with the user's name to the comments array
+      setComments([...comments, commentText]);
+      // Clear the comment input
+      setCommentText('');
+    }
+  };
+
+  return (
+    <div className="w-752 h-185 bg-white p-4 mb-4 rounded-lg shadow-md mx-auto" style={{ maxWidth: '752px' }}>
+      <div className="flex items-center space-x-2 mb-2">
+        <div className="text-blue-500 font-semibold">{user}</div>
+        <div className="text-gray-400">posted a post</div>
+      </div>
+      <p style={{fontFamily:'cursive'}}>{post.body}</p>
+      
+      <div className="flex justify-evenly items-center space-x-2 mt-4">
+        <button
+          className={`text-${liked ? 'blue' : 'gray'}-500 hover:text-blue-700`}
+          onClick={handleLikeClick}
+        >
+          <FontAwesomeIcon icon={faThumbsUp} /> Like
+        </button>
+        <button
+          className={`text-${disliked ? 'red' : 'gray'}-500 hover:text-red-700`}
+          onClick={handleDislikeClick}
+        >
+          <FontAwesomeIcon icon={faThumbsDown} /> Dislike
+        </button>
+        <button className="text-green-500 hover:text-green-700">
+          <FontAwesomeIcon icon={faComment} /> Comment
+        </button>
+      </div>
+
+     
+        {comments.map((comment, index) => (
+          <div key={index} className="rounded-lg p-3 mt-2" style={{ backgroundColor: '#F9F9F9', width:'264px', height:'56px', fontFamily:'cursive' }}>
+            <div className="font-semibold">{user}</div>
+            <div>{comment}</div>
+          </div>
+        ))}
+         {/* Comment section */}
+      <div className="mt-2">
+        <div className="flex items-center space-x-2">
+          <input
+            type="text"
+            className="border border-gray-300 rounded-lg p-2 w-full focus:outline-none focus:border-blue-500"
+            placeholder={`Add a comment as ${user}...`}
+            value={commentText}
+            onChange={handleCommentChange}
+          />
+          <button
+            className="bg-green-500 text-white px-3 py-1 rounded-md hover:bg-green-600 transition duration-300 ease-in-out"
+            onClick={handleCommentSubmit}
+          >
+            Post
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PostContent;
