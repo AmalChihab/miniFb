c6eccbc1d19f0fab81d67e99633dcb4f90c8f7b1
kao19 <feiyukine@gmail.com>
2023-10-08 11:42:58 +0100

    fixing deleting problem due to existing foreign keys

--- backend/src/main/java/irisi/facebook/backend/domain/repositories/CommentRepository.java
+++ backend/src/main/java/irisi/facebook/backend/domain/repositories/CommentRepository.java
@@ -1,7 +1,9 @@
 package irisi.facebook.backend.domain.repositories;
 
 import irisi.facebook.backend.domain.model.Comment;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -11,4 +13,10 @@ import java.util.List;
 public interface CommentRepository extends JpaRepository<Comment, Integer> {
     @Query("SELECT c FROM Comment c WHERE c.commentPost.postId = :postId")
     List<Comment> findByPostId(Long postId);
+
+    // added this one to make sure to delete all the comments for a post before deleting it
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM Comment c WHERE c.commentPost.postId = :postId")
+    void deleteReactionsByCommentPost_PostId(int postId);
 }
--- backend/src/main/java/irisi/facebook/backend/domain/repositories/ReactionRepository.java
+++ backend/src/main/java/irisi/facebook/backend/domain/repositories/ReactionRepository.java
@@ -1,8 +1,11 @@
 package irisi.facebook.backend.domain.repositories;
 
 import irisi.facebook.backend.domain.model.Reaction;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 
@@ -21,4 +24,9 @@ public interface ReactionRepository extends JpaRepository<Reaction,Integer> {
     @Query("SELECT r.reactionId FROM Reaction r WHERE r.postReaction.postId = :postId and r.reactingUser.userId = :userId")
     Integer getReactionIdByPostIdAndUserId(int postId, int userId);
 
+    // added this one to make sure to delete all the reactions for a post before deleting it
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM Reaction r WHERE r.postReaction.postId = :postId")
+    void deleteReactionsByPostReaction_PostId(int postId);
 }
